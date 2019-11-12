f76b703ed8e62a937c7829704c80d902675cc7d7
Raiondesu <a.raiondesu@gmail.com>
2019-11-13 01:22:23 +0300

    Update dist

--- dist/esnext/index.js
+++ dist/esnext/index.js
@@ -1,5 +1,7 @@
-export * from "./request.js";
+export * from "./fetch.js";
 export * from "./merge.js";
+export * from "./methods.js";
 export * from "./query.js";
+export * from "./request.js";
 export * from "./suburl.js";
 //# sourceMappingURL=index.js.map
