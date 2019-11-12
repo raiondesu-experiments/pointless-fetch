b6e3906df95e48af1fd825e292a32141163e5d80
Raiondesu <a.raiondesu@gmail.com>
2019-11-13 01:15:51 +0300

    Refactor src/request
    - `combine` now adheres to SOLID
    - TRequest type is generalized

--- src/request.ts
+++ src/request.ts
@@ -1,13 +1,31 @@
-import { join, isBase } from './util';
+import { mergeHeaders, mergeUrls, defaultMerge } from './merge';
 
-export type TRequest = RequestInit & { url: string };
+export type TRequest =
+  | RequestInit & { url: string }
+  | Request;
 
 export const request = ({ url, ...init }: TRequest) => new Request(url, init);
 
+export type TCombiner = {
+  (r: TRequest, addon: TRequest): Request;
+} & {
+  -readonly[key in keyof TRequest]?: (value1: TRequest[key], value2: TRequest[key]) => TRequest[key];
+};
+
 /**
- * Combines two Request configs
+ * Combines two Request configs using special merge strategies for each field.
+ *
+ * Each merge strategy is stored in a property with the same name
+ * as the corresponding Request property
+ * and can be replaced at any time.
  */
-export const combine = (request: Request, addon: TRequest | Request) => new Request({
-  ...request,
-  url: isBase(addon.url) ? addon.url : join(request.url, addon.url),
-}, addon);
+export const combine: TCombiner = (req1: TRequest, req2: TRequest) => request(
+  Object.keys(req1).concat(Object.keys(req2))
+  .reduce((res, key) => (
+    res[key] = (combine[key] || defaultMerge)(req1[key], req2[key]),
+    res
+  ), {} as TRequest)
+);
+
+combine.headers = mergeHeaders;
+combine.url = mergeUrls;
