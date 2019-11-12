7612f70b1c18a50f16538c234b9f602812a70636
Raiondesu <a.raiondesu@gmail.com>
2019-11-12 16:23:11 +0300

    Add [combine] src/request

--- src/request.ts
+++ src/request.ts
@@ -1,3 +1,13 @@
+import { join, isBase } from './util';
+
 export type TRequest = RequestInit & { url: string };
 
 export const request = ({ url, ...init }: TRequest) => new Request(url, init);
+
+/**
+ * Combines two Request configs
+ */
+export const combine = (request: Request, addon: TRequest | Request) => new Request({
+  ...request,
+  url: isBase(addon.url) ? addon.url : join(request.url, addon.url),
+}, addon);
