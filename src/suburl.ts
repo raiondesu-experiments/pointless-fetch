ffa4b5a238e1617591c27c72f939f6dc96c66898
Raiondesu <a.raiondesu@gmail.com>
2019-12-09 19:20:13 +0300

    Refactor [subUrl -> suburl] suburl - make code more readable

--- src/suburl.ts
+++ src/suburl.ts
@@ -1,13 +1,19 @@
-import { joinUrls } from './util';
+import { joinUrls, isBase } from './util';
+
+export type TSubUrlContext = { base: string; url?: string } | null | undefined;
 
 export type TSubUrlFactory = string & {
-  (base: string): typeof subUrl;
-
   <F extends (...args: any[]) => string>(
+    this: TSubUrlContext | void,
     base: F
-  ): (
-    ...args: Parameters<F>
-  ) => typeof subUrl;
+  ): (...args: Parameters<F>) => TSubUrlFactory;
+
+  (this: TSubUrlContext | void): TSubUrlFactory;
+
+  (
+    this: TSubUrlContext | void,
+    base: string
+  ): TSubUrlFactory;
 
   toString(): string;
 };
@@ -26,29 +32,52 @@ export type TSubUrlFactory = string & {
  *  // result:
  *  'https://my-blog.com/user'
  *
+ *  user.call({ base: 'https://google.com' })
+ *  // result:
+ *  'https://google.com/user'
+ *
+ *  user.call({ base: 'https://google.com', url: 'blog' })
+ *  // result:
+ *  'https://google.com/user/blog'
+ *
+ *  user.call({ base: 'https://google.com', url: 'blog' }, 'post')
+ *  // result:
+ *  'https://google.com/user/blog/post'
+ *
  *  const userPost = user((userId: number, postId: number) => `${userId}/post/${postId}`);
  *  // result:
  *  (userId: number, postId: number) => 'https://my-blog.com/user/${userId}/post/${postId}'
  * ```
  */
-export const subUrl = <TSubUrlFactory> function (
-  this: { base?: string },
-  url: string | ((...args: any[]) => string)
+export const suburl = <TSubUrlFactory> function (
+  this: TSubUrlContext,
+  url?: string | ((...args: any[]) => string)
 ) {
-  const applyBase = (
-    _url: (...args: any[]) => string
-  ) => (...args: any[]) => {
-    const concat = () => joinUrls(this?.base, _url(...args));
+  if (!url) {
+    return suburl;
+  }
 
-    const _subUrl = subUrl.bind({ base: concat() });
-    _subUrl.toString = concat;
+  if (typeof url === 'function') {
+    return (...args: any[]) => suburl.call(this, url(...args));
+  }
 
-    return _subUrl;
+  // If context is present, but trying to set a new base
+  // - reset the context
+  if (this?.base && isBase(url)) {
+    return suburl.call(void 0, url);
+  }
+
+  const context: TSubUrlContext = {
+    base: this?.base ?? url,
+    url: this?.base
+      ? joinUrls(this.url, url)
+      : '',
   };
 
-  return typeof url === 'string'
-    ? applyBase(() => url)()
-    : applyBase(url);
+  const _subUrl = suburl.bind(context);
+  _subUrl.toString = () => joinUrls(context.base, context.url);
+
+  return _subUrl;
 };
 
-subUrl.toString = () => '';
+suburl.toString = () => '';
